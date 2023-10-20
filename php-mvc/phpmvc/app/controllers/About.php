071b0dc705fe57f3082baeb303d68209084b70e5
00-mikazuki <diojuan432@gmail.com>
2023-10-20 22:25:23 +0700

    php-mvc

---
+++ php-mvc/phpmvc/app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About {
+  public function index($nama = 'Juan', $pekerjaan = 'Mahasiswa') {
+    echo "Halo, nama saya $nama, saya adalah $pekerjaan";
+  }
+  public function page() {
+    echo "About/page";
+  }
+}
